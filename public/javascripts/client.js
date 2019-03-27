70a96f2b7a2531920334975c47c5b5030f063b74
TANAKA-Makoto <45248369+TANAKA-Makoto@users.noreply.github.com>
2019-03-27 12:42:13 +0900

    self camera stop button

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,6 +7,7 @@ function startVideo(){
   navigator.getUserMedia({video: true, audio: false}, 
     function(stream) { // for success case
       console.log(stream);
+      localStream = stream;
       /* video.src = window.URL.createObjectURL(stream); 
        Chrome68以降で URL.createObjectURL が使えなくなったらしいので、
        以下のsrcObjectプロパティを使うように修正 */
@@ -15,4 +16,33 @@ function startVideo(){
     function(err) { // for error case
       console.log(err);
     });
+}
+
+  // stop local video
+function stopVideo() {
+  pauseVideo(localVideo);
+  stopLocalStream(localStream);
+}
+function pauseVideo(element) {
+  element.pause();
+  if ('srcObject' in element) {
+    element.srcObject = null;
+  }
+  else {
+    if (element.src && (element.src !== '') ) {
+      window.URL.revokeObjectURL(element.src);
+    }
+    element.src = '';
+  }
+}
+function stopLocalStream(stream) {
+  let tracks = stream.getTracks();
+  if (! tracks) {
+    console.warn('NO tracks');
+    return;
+  }
+  
+  for (let track of tracks) {
+    track.stop();
+  }
 }
