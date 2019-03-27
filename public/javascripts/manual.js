b4098a00b1714a1927f55bc6831cd49c2b3c3206
TANAKA-Makoto <45248369+TANAKA-Makoto@users.noreply.github.com>
2019-03-27 23:40:39 +0900

    camera selector in manual page

--- public/javascripts/manual.js
+++ public/javascripts/manual.js
@@ -1,10 +1,14 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
+  const deviceSelector = document.getElementById('deviceSelector');
   let localVideo = document.getElementById('local_video');
   let remoteVideo = document.getElementById('remote_video');
   let localStream = null;
   let peerConnection = null;
   let textForSendSdp = document.getElementById('text_for_send_sdp');
   let textToReceiveSdp = document.getElementById('text_for_receive_sdp');
+  let constraints = {
+                    video: true,
+                    audio: false};
 
   // --- prefix -----
   navigator.getUserMedia  = navigator.getUserMedia    || navigator.webkitGetUserMedia ||
@@ -13,17 +17,40 @@
   RTCSessionDescription = window.RTCSessionDescription || window.webkitRTCSessionDescription || window.mozRTCSessionDescription;
 
   // ---------------------- media handling ----------------------- 
-  // start local video
-  function startVideo() {
-    getDeviceStream({video: true, audio: false})
-    .then(function (stream) { // success
-      localStream = stream;
-      playVideo(localVideo, stream);
-    }).catch(function (error) { // error
-      console.error('getUserMedia error:', error);
-      return;
+  navigator.mediaDevices.enumerateDevices()
+  .then(function(devices) { // 成功時
+    devices.forEach(function(device) {
+      console.log(device);
+      const deviceID=device.deviceId;
+      if (device.kind === "videoinput") {
+        newChild = document.createElement('option');
+        newChild.textContent = device.label;
+        newChild.setAttribute('data-deviceID', deviceID);
+        deviceSelector.appendChild(newChild);
+      }
     });
-  }
+  })
+  .catch(function(err) { // エラー発生時
+    console.error('enumerateDevide ERROR:', err);
+  });
+  // start local video
+function startVideo(){
+  const num = deviceSelector.selectedIndex;
+  const deviceID = deviceSelector.options[num].getAttribute('data-deviceID');
+  constraints.video = {deviceId: {exact: deviceID}};
+  navigator.getUserMedia(constraints, 
+    function(stream) { // for success case
+      console.log(stream);
+      localStream = stream;
+      /* video.src = window.URL.createObjectURL(stream); 
+       Chrome68以降で URL.createObjectURL が使えなくなったらしいので、
+       以下のsrcObjectプロパティを使うように修正 */
+      localVideo.srcObject = stream;
+    },
+    function(err) { // for error case
+      console.log(err);
+    });
+}
 
   // stop local video
   function stopVideo() {
