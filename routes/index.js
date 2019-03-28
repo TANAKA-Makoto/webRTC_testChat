8b88627572b7e373f1797a01294a9b0004dafc6f
TANAKA-Makoto <45248369+TANAKA-Makoto@users.noreply.github.com>
2019-03-28 18:49:46 +0900

    add top page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Self Camera' });
+  res.render('index', { title: 'webRTC test' });
 });
 /* GET camera echo page. */
 router.get('/self', function(req, res, next) {
