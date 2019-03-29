5d9a66188e94487ab7a5860fc56d86e6c1c17674
TANAKA-Makoto <45248369+TANAKA-Makoto@users.noreply.github.com>
2019-03-29 18:14:27 +0900

    add route and jade

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const socketServerURL="http://192.168.0.28:9001";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const child     = ['/self', '/manual', '/auto', '/trickle', '/self_Org'];
+  const child     = ['/self', '/manual', '/auto', '/trickle', '/self_Org', 'multi'];
   const pageTitle = ['self camera echo',
                      'manual hand shake',
                      'auto hand shake',
                      'trickle hand shake',
-                     'Self Camera Org'];
+                     'Self Camera Org',
+                     'Multi Chat'];
   res.render('index', { title: 'webRTC test', child: child, pageTitle:pageTitle});
 });
 /* GET camera echo page. */
@@ -30,5 +32,9 @@ router.get('/trickle', function(req, res, next){
 /* GET camera echo ORIGINAL page. */
 router.get('/self_Org', function(req, res, next) {
   res.render('self', { title: 'Self Camera Org' });
+});
+/* GET camera echo ORIGINAL page. */
+router.get('/multi', function(req, res, next) {
+  res.render('multi', { title: 'Multi Chat', socketServerURL: socketServerURL });
 });
 module.exports = router;
